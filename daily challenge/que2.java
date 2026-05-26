3d36f3ad95a5a117b77fe35fe8bbe525a999a9e6
RUPESH KUMAR SAH <sahr89736@gmail.com>
2026-05-26 23:00:51 +0530

    solve count-the-number-of-special-characters-i(today problem easy)

---
+++ daily challenge/que2.java
@@ -0,0 +1,70 @@
+-----------------------------------count-the-number-of-special-characters-i--------------------------------------------
+// You are given a string word. A letter is called special if it appears both in lowercase and uppercase in word.
+
+// Return the number of special letters in word.
+
+ 
+
+// Example 1:
+
+// Input: word = "aaAbcBC"
+
+// Output: 3
+
+// Explanation:
+
+// The special characters in word are 'a', 'b', and 'c'.
+
+// Example 2:
+
+// Input: word = "abc"
+
+// Output: 0
+
+// Explanation:
+
+// No character in word appears in uppercase.
+
+// Example 3:
+
+// Input: word = "abBCab"
+
+// Output: 1
+
+// Explanation:
+
+// The only special character in word is 'b'.
+
+ 
+
+// Constraints:
+
+// 1 <= word.length <= 50
+// word consists of only lowercase and uppercase English letters.
+
+------------------------------------------Solution-----------------------------------------
+
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        boolean[] lower = new boolean[26];
+        boolean[] upper = new boolean[26];
+
+        for (char ch : word.toCharArray()) {
+            if (Character.isLowerCase(ch)) {
+                lower[ch - 'a'] = true;
+            } else {
+                upper[ch - 'A'] = true;
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if (lower[i] && upper[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
